0e77ce08ef210062b22f2de688bbec320ce2e27c
jiyeon.lizzy <jiyeon.lizzy@in-comms.co.kr>
2022-04-13 12:31:32 +0900

    upload images

--- shama-project/src/components/Main.js
+++ shama-project/src/components/Main.js
@@ -3,7 +3,7 @@ import Screen from './Screen'
 
 
 const Main = () => {
-    const FrontImage = require('./../img/front.png')
+    const FrontImage = require('./../img/front2.png')
     const [isActive, setIsActive] = useState(false)
     const ToggleClass = () => {
         setIsActive(!isActive)
@@ -15,6 +15,11 @@ const Main = () => {
                 <div className="imgWrap">
                     <img src={FrontImage} width="800" height="auto" alt="다마고치"/>
                     <Screen ToggleClass={ToggleClass} isActive={isActive}/>
+                    {/* <div className="controlBtn_wrap">
+                        <button type="button" className="btn_01"></button>
+                        <button type="button" className="btn_02"></button>
+                        <button type="button" className="btn_03"></button>
+                    </div> */}
                 </div>
                 <button type="button" className="pin" onClick={ToggleClass}><span className="text">PULL</span></button>
             </div>
