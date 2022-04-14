16e661502923c41e633d79f749fb0e1a70cf9d7b
jiyeon.lizzy <jiyeon.lizzy@in-comms.co.kr>
2022-04-14 11:40:32 +0900

    add ghost img

--- shama-project/src/components/Main.js
+++ shama-project/src/components/Main.js
@@ -6,19 +6,21 @@ const Main = () => {
     const FrontImage = require('./../img/front2.png')
     const [isActive, setIsActive] = useState(false)
     const ToggleClass = () => {
-        setIsActive(!isActive)
+        if(!isActive){
+            setIsActive(!isActive)
+        }
     }
     return (
         <div className="wrapper">
             {/* <Intro/> */}
             <div className="device">
                 <div className="imgWrap">
-                    <img src={FrontImage} width="800" height="auto" alt="다마고치"/>
+                    <img src={FrontImage} width="800" height="auto" className="deviceImg" alt="다마고치"/>
                     <Screen ToggleClass={ToggleClass} isActive={isActive}/>
                     <div className="controlBtn_wrap">
-                        <button type="button" className={"btn_01 " + (isActive ? 'active' : '')} onClick={ToggleClass}></button>
-                        <button type="button" className={"btn_02 " + (isActive ? 'active' : '')} onClick={ToggleClass}></button>
-                        <button type="button" className={"btn_03 " + (isActive ? 'active' : '')} onClick={ToggleClass}></button>
+                        <button type="button" className="btn_01"></button>
+                        <button type="button" className="btn_02"></button>
+                        <button type="button" className="btn_03"></button>
                     </div>
                     <button type="button" className="pin" onClick={ToggleClass}><span className="text">PULL</span></button>
                 </div>
--- shama-project/src/components/Screen.js
+++ shama-project/src/components/Screen.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
-const Screen = ({isActive}) => {
-    
+const Screen = ({isActive, ToggleClass}) => {
+    const GhostImage = require('./../img/shy_who.png')
     return (
         <div className="screen">
             <p className={"comingSoon " + (isActive ? 'active' : '')}><span className="vh">Coming Soon</span></p>
+            <img src={GhostImage} onClick={ () => ToggleClass()} className={"ghostImg " + (isActive ? 'active' : '')} alt="고스트실루엣"/>
+            <img src={GhostImage} onClick={ () => ToggleClass()} className={"ghostImg " + (isActive ? 'active' : '')} alt="고스트실루엣"/>
+            <img src={GhostImage} onClick={ () => ToggleClass()} className={"ghostImg " + (isActive ? 'active' : '')} alt="고스트실루엣"/>
         </div>
     );
 };
