3b2def90af7b69d8a7924e79488e7c250e9fd11b
ShamaJY <pj01102@naver.com>
2022-04-22 18:07:58 +0900

    add audio

--- shama-project/src/components/Main.js
+++ shama-project/src/components/Main.js
@@ -1,7 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Screen from './Screen'
 import Draggable from 'react-draggable'
 import './style.scss';
+import sound from './../audio/btn_sound.mp3'
+import BgSound from './../audio/shamagotchi_bgm.mp3'
 
 
 const Main = () => {
@@ -18,6 +20,21 @@ const Main = () => {
         setPosition({ y: data.y }); 
         ToggleClass()
     };
+    useEffect(() => {
+        playBGSound()
+    },[isActive])
+    const playBGSound = () => {
+        BackgroundSound.play()
+    }
+    const playSound = () => {
+        btnSound.play()
+    }
+      // 아무 버튼을 누르시오 이후 실행될 function
+      const trigger = (e) => {
+        playSound()
+    }
+    const btnSound = new Audio(sound)
+    const BackgroundSound = new Audio(BgSound)
     return (
         <div className="wrapper">
             <div className="device">
@@ -25,9 +42,9 @@ const Main = () => {
                     <img src={FrontImage} width="800" height="auto" className="deviceImg" alt="다마고치"/>
                     <Screen ToggleClass={ToggleClass} isActive={isActive}/>
                     <div className="controlBtn_wrap">
-                        <button type="button" className="left"><span className="vh">left</span></button>
-                        <button type="button" className="action"><span className="vh">action</span></button>
-                        <button type="button" className="right"><span className="vh">right</span></button>
+                        <button type="button" className="left" onClick={(e) => trigger("action")}><span className="vh">left</span></button>
+                        <button type="button" className="action" onClick={(e) => trigger("action")}><span className="vh">action</span></button>
+                        <button type="button" className="right" onClick={(e) => trigger("action")}><span className="vh">right</span></button>
                     </div>
                     <Draggable axis="y" onDrag={(e, data) => trackPos(data)} bounds={{top:0,bottom:20}}>
                         <button type="button" className="pin" onClick={ToggleClass}><span className='vh'>pull</span></button>
