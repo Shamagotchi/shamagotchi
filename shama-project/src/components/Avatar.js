b0b99a167e922286e9fb7a80118d7a5738c258b7
ShamaJY <pj01102@naver.com>
2022-04-22 17:59:14 +0900

    add audio

--- shama-project/src/components/Avatar.js
+++ shama-project/src/components/Avatar.js
@@ -2,30 +2,34 @@ import React, {useEffect,useState} from 'react';
 import Avatar1 from './../img/867.png';
 import Cloud from './../img/cloud.png'
 
-const Avatar = ({action, count, isStart}) => {
+const Avatar = ({count, isStart}) => {
     const [isCloud, setIsCloud ] = useState(true)
-    const say = [
+    const saying = [
         'Hi','Grrrr....','?','!!!!'
     ]
+    const [say, setSay] = useState('')
     if(isStart){
         setTimeout(() => {
             setIsCloud(false)
         },10000)
     }
-    console.log(action)
-    const randomSay = say[Math.floor(Math.random() * say.length)]
-    // useEffect(() => {
-        
-    // },[action])
+    useEffect(() => {
+        let changeSaying = () => {
+            setSay(saying[Math.floor(Math.random() * saying.length)])
+        }
+        setTimeout(() => {
+            changeSaying()
+        },[10000])
+    },[])
+
+
     return (
         <div className='avatar' style={{left : `${count * 5}%`}}>
             <img src={Avatar1} className="character" alt="avatar"/>
             {
                 isCloud ? <img className='cloud' src={Cloud} alt="cloud"/> : null
             }
-            {
-                action ?  <p className='bubble'>{randomSay}</p> : null
-            }
+            <p className='bubble'>{say}</p>
            
         </div>
     );
--- shama-project/src/components/Main.js
+++ shama-project/src/components/Main.js
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import Screen from './Screen'
 import Draggable from 'react-draggable'
-import './style.scss';
+import './style.scss'
+import sound from './../audio/btn_sound.mp3'
+import BgSound from './../audio/shamagotchi_bgm.mp3'
+
 
 
 const Main = () => {
@@ -15,17 +18,28 @@ const Main = () => {
             setIsActive(!isActive)
         }
     }
+    const btnSound = new Audio(sound)
+    const BackgroundSound = new Audio(BgSound)
     const [position, setPosition] = useState({ y: 0 });
     // 업데이트 되는 값을 set 해줌
     const trackPos = (data,bounds) => {
         setPosition({ y: data.y }); 
         ToggleClass()
     };
-
+    useEffect(() => {
+        playBGSound()
+    },[isActive])
+    const playBGSound = () => {
+        BackgroundSound.play()
+    }
+    const playSound = () => {
+        btnSound.play()
+    }
     // 아무 버튼을 누르시오 이후 실행될 function
     const trigger = (e) => {
         setIsActive(false)
         setIsStart(true)
+        playSound()
         if(e === "left"){
             setCount(count-1)
         }else if(e === "right"){
@@ -33,6 +47,7 @@ const Main = () => {
         }else if(e === "action"){
             setAction(true)
         }
+
     }
 
     // 키보드 동작 
@@ -64,9 +79,10 @@ const Main = () => {
                     <img src={FrontImage} width="800" height="auto" className="deviceImg" alt="다마고치"/>
                     <Screen ToggleClass={ToggleClass} action={action} isActive={isActive} isStart={isStart} count={count}/>
                     <div className="controlBtn_wrap">
-                        <button type="button" className="left" onClick={ (e) => trigger("left")}><span className="vh">left</span></button>
+                        <button type="button" className="left" onClick={(e) => trigger("action")}><span className="vh">left</span></button>
                         <button type="button" className="action" onClick={(e) => trigger("action")}><span className="vh">action</span></button>
                         <button type="button" className="right" onClick={(e) => trigger("right")}><span className="vh">right</span></button>
+
                     </div>
                     <Draggable axis="y" onDrag={(e, data) => trackPos(data)} bounds={{top:0,bottom:40}}>
                         <button type="button" className="pin" onClick={ToggleClass}><span className='vh'>pull</span></button>
