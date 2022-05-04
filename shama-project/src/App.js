859749f1b8377e62f3f64a0e86cfbd4bd3165289
ShamaJY <pj01102@naver.com>
2022-05-04 16:53:03 +0900

    add basename

--- shama-project/src/App.js
+++ shama-project/src/App.js
@@ -9,7 +9,7 @@ import NotFound from './components/NotFound';
 const App = () => {
   return (
     <>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
           <Route path="/" element={<Main/>}></Route>
           <Route path="/:Id" element={<Main/>}></Route>
