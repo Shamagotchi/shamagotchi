43f5f6cca12b51e53071097697bcb6c7f610aa89
ShamaJY <pj01102@naver.com>
2022-05-27 16:30:00 +0900

    add teach saying function

--- shama-project/src/components/Menu/Menu.js
+++ shama-project/src/components/Menu/Menu.js
@@ -5,37 +5,43 @@ import './Menu.scss'
 const Menu = ({say, setSay, findMyGhost}) => {
     const ref = useRef()
     const [changeSaying, setChangeSaying] = useState({
-        saying : ''
+        sayingText : ''
     })
-    const {saying} = changeSaying
-    const changeVal = (e) => {
-        const {value} = e.target
+    const {sayingText} = changeSaying
+    const changeVal = (e) => {  
+        const {value} = e.target 
         setChangeSaying({
             ...changeSaying,
-            name : value
+            sayingText : value
         })
        
     }
     const onAdd = (e) => {
-        e.preventDefault()
+        if(!sayingText){
+            return
+        }else{
+            findMyGhost.saying.push(sayingText)
+        }
+        console.log(findMyGhost.saying)
         //공백처리
-        if(!saying) return
-        findMyGhost.saying.push(changeSaying.saying)
-        setChangeSaying({
-            saying : ''
-        })
-        ref.current.focus()
-        alert(changeSaying.saying)
+        // if(!saying) return
+        // findMyGhost.saying.push(changeSaying.saying)
+        // setChangeSaying({
+        //     saying : ''
+        // })
+        // ref.current.focus()
+        // alert(changeSaying.saying)
     }
 
     return (
         <div className="menu">
             <Typed className="subject" strings={['Teach your ghost to say!']} showCursor={false}
                     typeSpeed={80}/>
-            <form onSubmit={onAdd} >
-                <input type="text" name="saying" maxLength="5" autoFocus={true} className="CustomText" onChange={changeVal} ref={ref}/>
-                <button type="submit">Teach!</button>
-            </form>
+            <div className="sayingWrap">
+                <input type="text" name="sayingText" maxLength="5" autoFocus={true} className="CustomText" onChange={changeVal} ref={ref}/>
+                <button type='button' className='btnAdd' onClick={onAdd}>ADD</button>
+            </div>
+            <button type="button" className='btnListAdd'>+</button>
         </div>
     );
 };
