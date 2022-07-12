625f1cc20f569519c4265bddaf00b853e63dac08
jiyeonlee <dlwldus12341234@gmail.com>
2022-07-12 10:56:40 +0900

    fix bug - saying button

--- shama-project/src/components/Menu/Activity.js
+++ shama-project/src/components/Menu/Activity.js
@@ -6,9 +6,12 @@ const Activity = ({sayingArr, say, setSay,findMyGhost, menuActive, isMenuOn}) =>
     const sayImage = require('../../img/chat.png') // say 이미지 
     return (
         <div className="selectBox">
-            <button type="button" onClick={() => menuActive()} className="say">
+            {
+                findMyGhost && <button type="button" onClick={() => menuActive()} className="say">
                 <img src={sayImage} width="20" alt="say"/>
             </button>
+            }
+    
             {
                 isMenuOn && <Menu sayingArr={sayingArr} say={say} setSay={setSay} findMyGhost={findMyGhost}/>
             }
