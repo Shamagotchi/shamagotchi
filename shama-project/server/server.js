2752ec35a9c6dcc125b395d2a10987d888915a05
jiyeonlee <dlwldus12341234@gmail.com>
2022-07-14 14:49:49 +0900

    connect test db

--- shama-project/server/server.js
+++ shama-project/server/server.js
@@ -1,11 +1,30 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
-app.get('/api/host', (req, res) => {
-    res.send({ host : 'shama' });
-})
+const path = require('path');
+const cors = require('cors');
+const db = require('./config/db');
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json())
+app.use(cors())
 
 app.listen(PORT, () => {
-  console.log(`Server On : http://localhost:${PORT}/`);
+    console.log(`Server On : http://localhost:${PORT}/`);
 })
 
+app.use(express.static(path.join(__dirname,'../build')))
+
+app.get('/', function(req,res){
+    res.sendFile(path.join(__dirname, '../build/index.html'))
+})
+
+app.get('/api', function(req,res){
+    db.query("SELECT * FROM test", (err, data) => {
+        if(!err) res.send(data);
+        else res.send(err);
+    })
+})
+
+app.get('*', function(req,res){
+    res.sendFile(path.join(__dirname, '../build/index.html'))
+})
--- shama-project/src/App.js
+++ shama-project/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{axios, Component} from 'react';
 import Main from './components/Main';
 import {HashRouter, Routes, Route, Router}  from "react-router-dom";
 import './style/normalize.css'
@@ -6,16 +6,35 @@ import './style/reset.css'
 import NotFound from './components/NotFound';
 
 
-const App = () => {
-  return (
-    <HashRouter>
-        <Routes>
-          <Route path="/" element={<Main/>}></Route>
-          <Route exact path="/:Id" element={<Main/>}></Route>
-          <Route path="*" element={<NotFound/>}></Route>
-        </Routes>
-    </HashRouter>
-  );
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      hello : [],
+    }
+  }
+  componentDidMount() {
+    this._getHello();
+  }
+
+  _getHello = async() => {
+    const res = await axios.get('/hello');
+    this.setState({ hello : res.data.hello })
+    console.log(this.state.hello);
+  }
+
+  render() {
+    return (
+      <HashRouter>
+          <Routes>
+            <Route path="/" element={<Main/>}></Route>
+            <Route exact path="/:Id" element={<Main/>}></Route>
+            <Route path="*" element={<NotFound/>}></Route>
+
+          </Routes>
+      </HashRouter>
+    )
+  }
 };
 
 export default App;
